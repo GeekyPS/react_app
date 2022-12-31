c6d64bb90d5d8a0ad2356e964caccc50be87ba72
Priyanshu Srivastava <priyanshusrivastava42@gmail.com>
2022-12-31 16:40:48 +0530

    third

---
+++ src/components/expense_item.js
@@ -0,0 +1,17 @@
+import './expense_item.css';
+function ExpenseItem(props) {
+    console.log(props);
+  
+
+  return 'hello';
+//     <div className='expense-item'>
+//       <div>{props.date}</div>
+//       <div className='expense-item__description'>
+//         <h2>{props.title}</h2>
+//         <div className='expense-item__price'>${props.amount}</div>
+//       </div>
+//     </div>
+//   );
+}
+
+export default ExpenseItem;
